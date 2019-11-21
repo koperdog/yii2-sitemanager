38e3baf41c400d43b1488dfbc226166d86a4fd58
checnev <checnev1997@yandex.ru>
2019-11-21 16:58:44 +0500

    prefriday commit

--- views/default/index.php
+++ views/default/index.php
@@ -9,8 +9,6 @@ use yii\widgets\ActiveForm;
     <?= Html::a(Yii::t('settings', 'Create Setting'), ['create'], ['class' => 'btn btn-success']) ?>
 </p>
 <?php $form = ActiveForm::begin(); ?>
-    <?php foreach ($settings as $index => $setting):?>
-        <?=$form->field($setting, "[$index]value")->label($setting->name);?>
-    <?php endforeach; ?>
+    <?php koperdog\yii2settings\widgets\SettingsForm::widget(); ?>
     <?= Html::submitButton(Yii::t('settings', 'Save'), ['class' => 'btn btn-primary']) ?>
 <?php ActiveForm::end();?>
